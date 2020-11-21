6046f20cedb483cb822c3bf6a492bb843ff8bd27
Thomas <t.pernetcoudrier@gmail.com>
2020-11-21 12:37:07 +0100

    Add notebook Epic Epic 0 US US 3 Upload city policy reduction mandate data to S3
    akb70xvku99812i

---
+++ 01_data_preprocessing/00_download_data_from/CITY_REDUCTION_MANDATE/city_reduction_mandate.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from awsPy.aws_s3 import service_s3
+from awsPy.aws_glue import service_glue
+from awsPy.aws_authorization import aws_connector
+from GoogleDrivePy.google_drive import connect_drive
+from GoogleDrivePy.google_authorization import authorization_service
+from pathlib import Path
+import os
+import json
+from tqdm import tqdm
+# Download stata file
+path = os.getcwd()
+parent_path = str(Path(path).parent.parent.parent)
+name_credential = 'financial_dep_SO2_accessKeys.csv'
+region = 'eu-west-3'
+bucket = 'datalake-datascience'
+path_cred = "{0}/creds/{1}".format(parent_path, name_credential)
+
+
+con = aws_connector.aws_instantiate(credential=path_cred,
+                                    region=region)
+client = con.client_boto()
+s3 = service_s3.connect_S3(client=client,
+                           bucket=bucket, verbose=True)
+
+# Create schema
+# Load schema from
+# https://docs.google.com/spreadsheets/d/1gfdmBKzZ1h93atSMFcj_6YgLxC7xX62BCxOngJwf7qE
+project = 'valid-pagoda-132423'
+auth = authorization_service.get_authorization(
+    path_credential_gcp="{}/creds/service.json".format(parent_path),
+    path_credential_drive="{}/creds".format(parent_path),
+    verbose=False
+)
+
+gd_auth = auth.authorization_drive()
+drive = connect_drive.drive_operations(gd_auth)
+
+spreadsheet_id = drive.find_file_id('China_cities_target_so2', to_print=False)
+var = (
+    drive.upload_data_from_spreadsheet(
+        sheetID=spreadsheet_id,
+        sheetName="China_cities_target_so2",
+        to_dataframe=True)
+)
+
+var.to_csv('China_cities_target_so2.csv', index = False)
+
+s3.upload_file('China_cities_target_so2.csv',
+"DATA/ENVIRONMENT/CHINA/FYP/CITY_REDUCTION_MANDATE")
+os.remove('TCZ_list_china')
