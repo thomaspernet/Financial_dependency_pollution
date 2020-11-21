209f36326894886d89beb88e3bfe184bf8da98f8
Thomas <t.pernetcoudrier@gmail.com>
2020-11-21 07:26:03 +0100

    Add notebook Epic Epic 0 US US 1 Upload city sector pollution data to S3
    khc09jwtl17047z

---
+++ 01_data_preprocessing/00_download_data_from/CITY_SECTOR_POLLUTION/city_sector_pollution.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from awsPy.aws_s3 import service_s3
+from awsPy.aws_glue import service_glue
+from awsPy.aws_authorization import aws_connector
+from GoogleDrivePy.google_drive import connect_drive
+from GoogleDrivePy.google_authorization import authorization_service
+from pathlib import Path
+import os
+import json
+from tqdm import tqdm
+# Download stata file
+path = os.getcwd()
+parent_path = str(Path(path).parent.parent.parent)
+name_credential = 'financial_dep_SO2_accessKeys.csv'
+region = 'eu-west-3'
+bucket = 'datalake-datascience'
+path_cred = "{0}/creds/{1}".format(parent_path, name_credential)
+
+
+con = aws_connector.aws_instantiate(credential=path_cred,
+                                    region=region)
+client = con.client_boto()
+s3 = service_s3.connect_S3(client=client,
+                           bucket=bucket, verbose=True)
+
+# Create schema
+# Load schema from
+# https://docs.google.com/spreadsheets/d/1gfdmBKzZ1h93atSMFcj_6YgLxC7xX62BCxOngJwf7qE
+project = 'valid-pagoda-132423'
+auth = authorization_service.get_authorization(
+    path_credential_gcp="{}/creds/service.json".format(parent_path),
+    path_credential_drive="{}/creds".format(parent_path),
+    verbose=False
+)
