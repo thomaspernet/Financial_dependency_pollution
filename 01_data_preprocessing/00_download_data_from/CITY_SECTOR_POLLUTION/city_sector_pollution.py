f748f40c954e9862761402093da55eaf5f78d0b4
Thomas <t.pernetcoudrier@gmail.com>
2020-11-21 08:11:21 +0100

    Dowwload from CGP and upload to S3
    khc09jwtl17047z

--- 01_data_preprocessing/00_download_data_from/CITY_SECTOR_POLLUTION/city_sector_pollution.py
+++ 01_data_preprocessing/00_download_data_from/CITY_SECTOR_POLLUTION/city_sector_pollution.py
@@ -2,7 +2,7 @@ import pandas as pd
 from awsPy.aws_s3 import service_s3
 from awsPy.aws_glue import service_glue
 from awsPy.aws_authorization import aws_connector
-from GoogleDrivePy.google_drive import connect_drive
+from GoogleDrivePy.google_platform import connect_cloud_platform
 from GoogleDrivePy.google_authorization import authorization_service
 from pathlib import Path
 import os
@@ -27,8 +27,24 @@ s3 = service_s3.connect_S3(client=client,
 # Load schema from
 # https://docs.google.com/spreadsheets/d/1gfdmBKzZ1h93atSMFcj_6YgLxC7xX62BCxOngJwf7qE
 project = 'valid-pagoda-132423'
-auth = authorization_service.get_authorization(
+gd_auth = authorization_service.get_authorization(
     path_credential_gcp="{}/creds/service.json".format(parent_path),
     path_credential_drive="{}/creds".format(parent_path),
     verbose=False
 )
+
+gcp_auth = auth.authorization_gcp()
+gcp = connect_cloud_platform.connect_console(project = project,
+                                                 service_account = gcp_auth)
+
+gcp.download_blob(
+bucket_name = "chinese_data",
+destination_blob_name = "Environmental_Statistics_china/Processed_",
+source_file_name = 'China_city_pollution_98_2007.gz')
+
+### Make sure of the content
+#pd.read_csv('China_city_pollution_98_2007.gz').head()
+
+### Save S3
+s3.upload_file('China_city_pollution_98_2007.gz', "DATA/ENVIRONMENT/CHINA/CITY_SECTOR_POLLUTION")
+os.remove('China_city_pollution_98_2007.gz')
