6ec65145fb1f1863b0ec248afaee2835a16293bc
Thomas <t.pernetcoudrier@gmail.com>
2020-11-21 08:26:15 +0100

    Add schema
    khc09jwtl17047z

--- 01_data_preprocessing/00_download_data_from/CITY_SECTOR_POLLUTION/city_sector_pollution.py
+++ 01_data_preprocessing/00_download_data_from/CITY_SECTOR_POLLUTION/city_sector_pollution.py
@@ -48,3 +48,155 @@ source_file_name = 'China_city_pollution_98_2007.gz')
 ### Save S3
 s3.upload_file('China_city_pollution_98_2007.gz', "DATA/ENVIRONMENT/CHINA/CITY_SECTOR_POLLUTION")
 os.remove('China_city_pollution_98_2007.gz')
+
+# Craw the table
+### Schema
+
+schema = [
+{
+    "Name": "year",
+    "Type": "string",
+    "Comment": ""
+},
+{
+    "Name": "prov2013",
+    "Type": "string",
+    "Comment": "Province name in Chinese"
+},
+{
+    "Name": "Provinces",
+    "Type": "string",
+    "Comment": "Province name in English"
+},
+{
+    "Name": "citycode",
+    "Type": "string",
+    "Comment": "citycode refers to geocode4corr"
+},
+{
+    "Name": "citycn",
+    "Type": "string",
+    "Comment": "City name in Chinese"
+},
+{
+    "Name": "cityen",
+    "Type": "string",
+    "Comment": "City name in English"
+},
+{
+    "Name": "indus_code",
+    "Type": "string",
+    "Comment": "4 digits industry code"
+},
+{
+    "Name": "ind2",
+    "Type": "string",
+    "Comment": "2 digits industry code"
+},
+{
+    "Name": "ttoutput",
+    "Type": "float",
+    "Comment": "Total output city sector"
+},
+{
+    "Name": "twaste_water",
+    "Type": "int",
+    "Comment": "Total waste water city sector"
+},
+{
+    "Name": "tCOD",
+    "Type": "float",
+    "Comment": "Total COD city sector"
+},
+{
+    "Name": "tAmmonia_Nitrogen",
+    "Type": "float",
+    "Comment": "Total Ammonia Nitrogen city sector"
+},
+{
+    "Name": "twaste_gas",
+    "Type": "int",
+    "Comment": "Total Waste gas city sector"
+},
+{
+    "Name": "tso2",
+    "Type": "int",
+    "Comment": "Total so2 city sector"
+},
+{
+    "Name": "tNOx",
+    "Type": "int",
+    "Comment": "Total NOx city sector"
+},
+{
+    "Name": "tsmoke_dust",
+    "Type": "int",
+    "Comment": "Total smoke dust city sector"
+},
+{
+    "Name": "tsoot",
+    "Type": "int",
+    "Comment": "Total soot city sector"
+},
+{
+    "Name": "Lower_location",
+    "Type": "string",
+    "Comment": "Location city. one of Coastal, Central, Northwest, Northeast, Southwest"
+},
+{
+    "Name": "Larger_location",
+    "Type": "string",
+    "Comment": "Location city. one of Eastern, Central, Western"
+},
+{
+    "Name": "Coastal",
+    "Type": "string",
+    "Comment": "City is bordered by sea or not"
+},
+
+]
+
+
+
+glue = service_glue.connect_glue(client=client)
+target_S3URI = "s3://datalake-datascience/DATA/ENVIRONMENT/CHINA/CITY_SECTOR_POLLUTION"
+name_crawler = "crawl-pollution"
+Role = 'arn:aws:iam::468786073381:role/AWSGlueServiceRole-crawler-datalake'
+DatabaseName = "environment"
+TablePrefix = 'china_'
+
+
+glue.create_table_glue(
+    target_S3URI,
+    name_crawler,
+    Role,
+    DatabaseName,
+    TablePrefix,
+    from_athena=False,
+    update_schema=schema,
+)
+
+# Add tp ETL parameter files
+json_etl = {
+    'description': 'Create Firms survey ASIF panel data from STATA',
+    'schema': schema,
+    'partition_keys': [],
+    'metadata': {
+        'DatabaseName': DatabaseName,
+        'TablePrefix': TablePrefix,
+        'target_S3URI': target_S3URI,
+        'from_athena': 'False'
+    }
+}
+
+path_to_etl = os.path.join(str(Path(path).parent.parent),
+                           'parameters_ETL_Financial_dependency_pollution.json')
+with open(path_to_etl) as json_file:
+    parameters = json.load(json_file)
+
+#parameters['TABLES']['CREATION']['ALL_SCHEMA'].pop(0)
+
+parameters['TABLES']['CREATION']['ALL_SCHEMA'].append(json_etl)
+
+with open(path_to_etl, "w")as outfile:
+    json.dump(parameters, outfile)
